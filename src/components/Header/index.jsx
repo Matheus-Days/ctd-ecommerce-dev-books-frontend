44f1bbd5089ed88a698ec31a8b787c441ac8d01c
Matheus Braga Dias <matheus@audo.com.br>
2022-04-20 17:59:30 -0300

    Finished header and footer; fixed aboutus

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,86 @@
-import "./styles.scss"
-import logo from "../../assets/logo.png"
-import { FaShoppingCart } from "react-icons/fa"
-import { RiAdminFill } from "react-icons/ri"
+import "./styles.scss";
+import logo from "../../assets/logo.png";
+import { FaShoppingCart } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiAdminFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { Offcanvas } from "react-bootstrap";
+import { useState } from "react";
 
 export default function Header() {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => {
+    show ? setShow(false) : setShow(true);
+  };
 
   return (
     <header>
       <Link to="/" className="logo">
-        <img src={logo} alt='logo dev books' />
+        <img src={logo} alt="logo dev books" />
         <h2>Dev Books</h2>
       </Link>
 
-      <nav>
+      <nav id="nav-ul">
+        <button className="toggle-btn" onClick={handleShow}>
+          <GiHamburgerMenu />
+        </button>
         <ul className="menu">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Produtos</Link></li>
-        <li><Link to="/categories">Categorias</Link></li>
-        <li><Link to="/aboutus">Sobre nós</Link></li>
-        <li><Link to="/cart"><FaShoppingCart className="Cart" /></Link></li>
-        <li><Link to="/admin"><RiAdminFill className="Adm" /></Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Produtos</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categorias</Link>
+          </li>
+          <li>
+            <Link to="/aboutus">Sobre nós</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <FaShoppingCart className="Cart" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin">
+              <RiAdminFill className="Adm" />
+            </Link>
+          </li>
         </ul>
       </nav>
 
+      <Offcanvas show={show} onHide={handleShow} placement="top">
+        <Offcanvas.Body id="header-offcanvas">
+          <ul className="offcanvas-menu">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <div className="divider"></div>
+            <li>
+              <Link to="/products">Produtos</Link>
+            </li>
+            <div className="divider"></div>
+            <li>
+              <Link to="/categories">Categorias</Link>
+            </li>
+            <div className="divider"></div>
+            <li>
+              <Link to="/aboutus">Sobre nós</Link>
+            </li>
+            <div className="divider"></div>
+            <li>
+              <Link to="/cart">
+                <FaShoppingCart className="Cart" />
+              </Link>
+              <Link to="/admin">
+                <RiAdminFill className="Adm" />
+              </Link>
+            </li>
+          </ul>
+        </Offcanvas.Body>
+      </Offcanvas>
     </header>
   );
-
 }
--- src/screens/AboutUs/index.jsx
+++ src/screens/AboutUs/index.jsx
@@ -1,12 +1,12 @@
-import './styles.scss';
-import { SiGithub } from 'react-icons/si';
-import { AiOutlineLinkedin } from 'react-icons/ai';
-import andre from '../../assets/profile/andre.png';
-import jehan from '../../assets/profile/jehan.jpeg';
-import marcia from '../../assets/profile/marcia.jpg';
-import matheus from '../../assets/profile/matheus.jpg';
-import renan from '../../assets/profile/renan.png';
-import vanessa from '../../assets/profile/vanessa.jpg';
+import "./styles.scss";
+import { SiGithub } from "react-icons/si";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import andre from "../../assets/profile/andre.png";
+import jehan from "../../assets/profile/jehan.jpeg";
+import marcia from "../../assets/profile/marcia.jpg";
+import matheus from "../../assets/profile/matheus.jpg";
+import renan from "../../assets/profile/renan.png";
+import vanessa from "../../assets/profile/vanessa.jpg";
 
 const data = [
   {
@@ -45,7 +45,6 @@ const data = [
     Linkedin: "https://www.linkedin.com/in/vanessa-matos-5a801416a/",
     image: vanessa,
   },
-
 ];
 
 export default function AboutUs() {
@@ -54,26 +53,46 @@ export default function AboutUs() {
       <div id="profile">
         <div className="profile-intro">
           <h1 className="profile-title">Time de Desenvolvedores Dev Books</h1>
-          <p className="profile-txt">Somos alunos do 3º bimestre do Curso de Programação Certified Tech Developer, pensado e desenhado pelo Mercado Livre, Globant e a Digital House.  Obrigado pela visita!</p>
+          <p className="profile-txt">
+            Somos alunos do 3º bimestre do Curso de Programação Certified Tech
+            Developer, pensado e desenhado pelo Mercado Livre, Globant e a
+            Digital House. Obrigado pela visita!
+          </p>
         </div>
 
         <ul className="card-list">
           {data.map(({ image, name, Linkedin, Git }) => (
-            <div>
-              <li className="item-list-card">
-                <img src={image} className="card-image" alt="foto dos integrantes" />
-                <div className="card-social">
-                  <h3 className="card-name">{name}</h3>
-                  <div className="card-media">
-                    <a className="card-git" href={Git} rel="noreferrer" target="_blank"><SiGithub color="black" /></a>
-                    <a className="card-linkedin" href={Linkedin} rel="noreferrer" target="_blank"><AiOutlineLinkedin color="black" /></a>
-                  </div>
+            <li className="item-list-card">
+              <img
+                src={image}
+                className="card-image"
+                alt="foto dos integrantes"
+              />
+              <div className="card-social">
+                <h3 className="card-name">{name}</h3>
+                <div className="card-media">
+                  <a
+                    className="card-git"
+                    href={Git}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <SiGithub color="black" />
+                  </a>
+                  <a
+                    className="card-linkedin"
+                    href={Linkedin}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <AiOutlineLinkedin color="black" />
+                  </a>
                 </div>
-              </li>
-            </div>
+              </div>
+            </li>
           ))}
         </ul>
       </div>
     </>
-  )
+  );
 }
